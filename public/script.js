5a20338c231579aa11cde90949d19add314565db
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-05-06 16:20:06 -0300

    reconfiguracao dos alerts e implementacao do menu dropdown

--- public/script.js
+++ public/script.js
@@ -1,14 +1,40 @@
-function closeAlert() {
+const closeAlert = () => {
   const close = document.querySelector("#close-mess");
   const message = document.querySelector(".message");
 
   close.addEventListener("click", () => {
-    message.style.display = "none";
+    message.style.display = "block";
   });
 
   setTimeout(() => {
     message.style.display = "none";
   }, 5000);
-}
+};
 
+const viewDropdown = () => {
+  const buttons = document.querySelectorAll(".dropdown-button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const content = event.path[2].children[1];
+      console.log(content);
+
+      content.classList.toggle("active");
+
+      if (content.classList.contains("active")) {
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
+      }
+
+      content.addEventListener("mouseleave", () => {
+        content.classList.remove("active");
+        if (!content.classList.contains("active")) {
+          content.style.display = "none";
+        }
+      });
+    });
+  });
+};
+
+viewDropdown();
 closeAlert();
